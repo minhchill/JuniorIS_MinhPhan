c6a50d0588bbd65936d90241ec8525849d7a03a9
Minh Chill <chieuminh0507@gmail.com>
2023-04-28 13:19:36 -0400

    Reorganization of files

--- Components/header.js
+++ Components/header.js
@@ -9,17 +9,17 @@ class Header extends HTMLElement {
           <meta charset="UTF-8">
           <meta http-equiv="X-UA-Compatible" content="IE=edge">
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Parallax Scrolling Website | Codehal</title>
+          <title>Chill's Sandbox</title>
           <link rel="stylesheet" href="Stylesheets/mainmenu.css">
         </head>  
         <header>
           <nav>
             <div class="logo">
-              <a href="#"><img src="Images/SandboxLogo.png"
+              <a href="mainmenu.html"><img src="Images/SandboxLogo.png"
             </div>
           </nav> 
           <nav class="navigation">
-              <a href="#" class="active">Home</a>
+              <a href="mainmenu.html" class="active">Home</a>
               <a href="clothsimulation_noninteractive.html">Simulation 1</a>
               <a href="clothsimulation.html">Simulation 2</a>
               <a href="https://github.com/minhchill/MuseumOfBanhMi.git">Source Code</a>
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # MuseumOfBanhMi
 
+The project can run on virtually all computer machines. 
+Please start exploring from the mainmenu.html since it will link to all the programs created during my research process
